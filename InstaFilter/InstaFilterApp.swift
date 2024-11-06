67c2072b40b03e8fb86aadea558a8315fccd0332
Kaloyan Stefanov <kokmarok@gmail.com>
2024-11-06 22:03:55 +0200

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Kok on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
